6e35789d4c8debd06a41b128a4f1eea1b6f0421e
LazRehn <LazRehn@nuuksio.com>
2022-05-13 14:07:48 +0300

    dummy commit to be ableto pull newest

--- SQL_calls.py
+++ SQL_calls.py
@@ -1,23 +1,23 @@
 import sqlite3
 from unittest import result
 
-sqlite_connection = sqlite3.connect('database.db')
-try:
-    sqlite_connection.execute('CREATE TABLE events (id TEXT, start TEXT, end TEXT, reg_nro TEXT, merkki TEXT, asiakas TEXT, puh_nro TEXT, tyomaarays TEXT)')
-    sqlite_connection.commit()
-except Exception as ex:
-    template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-    message = template.format(type(ex).__name__, ex.args)
-    print(ex.args) # Probably "The database table 'events' already exists. That's good!
-    # print(message)
-
+# Builds the list of events "db" from the database
 def load_db():
+    sqlite_connection = sqlite3.connect('database.db')
+    try:
+        sqlite_connection.execute('CREATE TABLE IF NOT EXISTS events (id TEXT, start TEXT, end TEXT, reg_nro TEXT, merkki TEXT, asiakas TEXT, puh_nro TEXT, tyomaarays TEXT)')
+        sqlite_connection.commit()
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
+    
     sqlite_connection.row_factory = sqlite3.Row #without this line, fetchall will return Tuples, not Row objects!
     sqlite_cursor = sqlite_connection.cursor()
     sqlite_cursor.execute('SELECT * FROM events ORDER BY start')
     db = [] # a list of dictionaries, to enable jsonify(db)
     for row in sqlite_cursor.fetchall():
-        # print(type(row)) # it should be sqlite.Row, not Tuple.
+        # print(type(row)) # it should be sqlite.Row (not Tuple).
         row_dictionary = {}
         row_keys = row.keys()
         row_values = tuple(row)
@@ -27,7 +27,7 @@ def load_db():
             row_dictionary[key] = row_values[value_index] #could there be a smarter way?
             value_index += 1
         db.append(row_dictionary)
-    sqlite_connection.close()
+    #sqlite_connection.close()  # not needed?
     return db
 
 def append_db(event):
@@ -46,7 +46,10 @@ def append_db(event):
             event["tyomaarays"] ) )
         sqlite_connection.commit()
         result = "OK"
-    except:
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
         result = "NOT_OK"
     return result
 
@@ -62,7 +65,10 @@ def move_db(event):
             event["id"] ))
         sqlite_connection.commit()
         result = "OK"
-    except:
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
         result = "NOT_OK"
     return result
 
@@ -74,7 +80,6 @@ def update_db(event):
     + "puh_nro = ?, "\
     + "tyomaarays = ? "\
     + "WHERE id = ?"
-
     try:
         sqlite_connection = sqlite3.connect('database.db')
         sqlite_cursor = sqlite_connection.cursor()
@@ -91,9 +96,8 @@ def update_db(event):
         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
         message = template.format(type(ex).__name__, ex.args)
         print(message)
-        
         result = "NOT_OK"
-        return result
+    return result
 
 def delete_db(event_id):
     try:
@@ -103,6 +107,9 @@ def delete_db(event_id):
         sqlite_cursor.execute(sql_string, (event_id,) )
         sqlite_connection.commit()
         result = "OK"
-    except:
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
         result = "NOT_OK"
     return result
